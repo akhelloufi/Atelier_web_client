96b472158c9341030f199456f7da6bbb05835aa0
abdelkarim khelloufi <khelloufi2006@gmail.com>
2021-02-03 18:34:00 +0100

    Update testDatabase.php

--- Atelier 3_ajax_autocompletion/site_ajax/testDatabase.php
+++ Atelier 3_ajax_autocompletion/site_ajax/testDatabase.php
@@ -2,9 +2,9 @@
  $connection = mysqli_connect("localhost","root","","villesmercredi")
   or die("Error " . mysqli_error($connection));
   //hostname localhost username :root, 
- 
+ $q=$_GET['q'];//testDatabase.php?q=" + str
     //fetch table rows from mysql db  
-       $sql = "select * from commune"; 
+       $sql = "select * from commune where nom1 like '".$q."%'"; 
            $result = mysqli_query($connection, $sql) 
            or die("Error in Selecting " . mysqli_error($connection));   
              //create an array    
